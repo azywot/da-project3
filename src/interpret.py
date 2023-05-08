793160143c43fe5c496b35df06309e303c62e3a6
azywot <agata.zywot@gmail.com>
2023-05-08 20:17:15 +0200

    add ann utadis

--- src/interpret.py
+++ src/interpret.py
@@ -1,5 +1,7 @@
 import numpy as np
 import xgboost as xgb
+import torch
+import torch.nn as nn
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 import shap
 
@@ -16,5 +18,12 @@ def shap_tree_explainer(model: xgb.XGBClassifier, x: np.ndarray,
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(x)
     np.abs(shap_values.sum(1) + explainer.expected_value - pred).max()
-    shap.summary_plot(shap_values, x)
+    shap.summary_plot(shap_values, x, color='coolwarm')
+    return explainer, shap_values
+
+def shap_deep_explainer(model: nn.Module, x: np.ndarray) -> tuple:
+    x_tensor = torch.from_numpy(x)
+    explainer = shap.DeepExplainer(model, x_tensor)
+    shap_values = explainer.shap_values(x_tensor)
+    shap.summary_plot(shap_values, x_tensor, color='coolwarm')
     return explainer, shap_values
